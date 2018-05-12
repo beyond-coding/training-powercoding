fe5109d47c82b3171e8d26568568362d363c874d
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2018-05-12 10:04:16 +0200

    Add project structure

---
+++ src/main/java/com/beyondcoding/trainingpowercoding/TrainingPowercodingApplication.java
@@ -0,0 +1,12 @@
+package com.beyondcoding.trainingpowercoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPowercodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPowercodingApplication.class, args);
+	}
+}
